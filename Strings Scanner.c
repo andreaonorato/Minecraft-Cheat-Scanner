ed75ef6121809d38349e887d2fda6b437ef3131a
pato240901 <63243990+pato240901@users.noreply.github.com>
2020-04-06 16:21:40 +0200

    Add files via upload

---
+++ Strings Scanner.c
@@ -0,0 +1,238 @@
+#include<stdio.h>
+#include<string.h>
+#include<windows.h>
+
+int main()
+
+{
+char* a= "/AVIX-Config";
+char* b= "trumpclientftw_bape";
+char* c= "dg82fo.pw";
+char* d= "nG@W";
+char* e= "G0ttaDipMen.java";
+char* f= "Harambe.png";
+char* g= "czaarek99";
+char* h= "+(M0G.V";
+char* i= "dinkio";
+char* l= "Sa_Vc";
+char* m= "5d@56";
+char* n= "TCNH$1";
+char* o= "onetap.cc";
+char* p= "bspkrs.IlIIIlIlIllIIlllIllIllIII";
+char* q= "com/sun/jna/z/Main";
+char* r= "/a.class:::0";
+char* s= "hi.a2";
+char* t= "0SO1Lk2KASxzsd";
+char* u= "yCcADi";
+char* v= "74.91.125.194";
+char* w= "kc(g9";
+char* x= "kc((k";
+char* y= "JNativeHook";
+char* k= "144.217.241.181";
+char* j= "/tcpnodelaymod/COM1";
+
+char a1[255]; 
+char b1[255];
+char c1[255];
+char d1[255];
+char e1[255];
+char f1[255];
+char g1[255];
+char h1[255];
+char i1[255];
+char l1[255];
+char m1[255];
+char n1[255];
+char o1[255];
+char p1[255];
+char q1[255];
+char r1[255];
+char s1[255];
+char t1[255];
+char u1[255];
+char v1[255];
+char w1[255];
+char x1[255];
+char y1[255];
+char k1[255];
+char j1[255];
+printf("Strings Scanner by DonTopoGigio_ (ONLY 1.8)\nWait a few minutes...\n");
+//AVIX a
+FILE* Filea;
+if((Filea = fopen("Search results.txt", "r"))==NULL)
+{	fprintf(stderr, "errore nell'apertura del file\n");
+}
+while(fgets(a1,254,Filea)) 
+printf("%s\n", strstr(a1, a)); 
+//TrumpClient b
+FILE* Fileb;
+if((Fileb = fopen("Search results.txt", "r"))==NULL)
+{	fprintf(stderr, "errore nell'apertura del file\n");
+}
+while(fgets(b1,254,Fileb))
+printf("%s\n", strstr(b1, b));
+//dg82fo.pw c
+FILE* Filec;
+if((Filec = fopen("Search results.txt", "r"))==NULL)
+{	fprintf(stderr, "errore nell'apertura del file\n");
+}
+while(fgets(c1,254,Filec))
+printf("%s\n", strstr(c1, c));
+//nG@W d
+FILE* Filed;
+if((Filed = fopen("Search results.txt", "r"))==NULL)
+{	fprintf(stderr, "errore nell'apertura del file\n");
+}
+while(fgets(d1,254,Filed))
+printf("%s\n", strstr(d1, d));
+//G0ttaDipMen.java e
+FILE* Filee;
+if((Filee = fopen("Search results.txt", "r"))==NULL)
+{	fprintf(stderr, "errore nell'apertura del file\n");
+}
+while(fgets(e1,254,Filee))
+printf("%s\n", strstr(e1, e));
+//Harambe.png f
+FILE* Filef;
+if((Filef = fopen("Search results.txt", "r"))==NULL)
+{	fprintf(stderr, "errore nell'apertura del file\n");
+}
+while(fgets(f1,254,Filef))
+printf("%s\n", strstr(f1, f));
+//czaarek99 g
+FILE* Fileg;
+if((Fileg = fopen("Search results.txt", "r"))==NULL)
+{	fprintf(stderr, "errore nell'apertura del file\n");
+}
+while(fgets(g1,254,Fileg))
+printf("%s\n", strstr(g1, g));
+//+(M0G.V h
+FILE* Fileh;
+if((Fileh = fopen("Search results.txt", "r"))==NULL)
+{	fprintf(stderr, "errore nell'apertura del file\n");
+}
+while(fgets(h1,254,Fileh))
+printf("%s\n", strstr(h1, h));
+//dinkio i
+FILE* Filei;
+if((Filei = fopen("Search results.txt", "r"))==NULL)
+{	fprintf(stderr, "errore nell'apertura del file\n");
+}
+while(fgets(i1,254,Filei))
+printf("%s\n", strstr(i1, i));
+//Sa_Vc l
+FILE* Filel;
+if((Filel = fopen("Search results.txt", "r"))==NULL)
+{	fprintf(stderr, "errore nell'apertura del file\n");
+}
+while(fgets(l1,254,Filel))
+printf("%s\n", strstr(l1, l));
+//5d@56 m
+FILE* Filem;
+if((Filem = fopen("Search results.txt", "r"))==NULL)
+{	fprintf(stderr, "errore nell'apertura del file\n");
+}
+while(fgets(m1,254,Filem))
+printf("%s\n", strstr(m1, m));
+//TCNH$1 n
+FILE* Filen;
+if((Filen = fopen("Search results.txt", "r"))==NULL)
+{	fprintf(stderr, "errore nell'apertura del file\n");
+}
+while(fgets(n1,254,Filen))
+printf("%s\n", strstr(n1, n));
+//onetap.cc o
+FILE* Fileo;
+if((Fileo = fopen("Search results.txt", "r"))==NULL)
+{	fprintf(stderr, "errore nell'apertura del file\n");
+}
+while(fgets(o1,254,Fileo))
+printf("%s\n", strstr(o1, o));
+//bspkrs.IlIIIlIlIllIIlllIllIllIII p
+FILE* Filep;
+if((Filep = fopen("Search results.txt", "r"))==NULL)
+{	fprintf(stderr, "errore nell'apertura del file\n");
+}
+while(fgets(p1,254,Filep))
+printf("%s\n", strstr(p1, p));
+//com/sun/jna/z/Main q
+FILE* Fileq;
+if((Fileq = fopen("Search results.txt", "r"))==NULL)
+{	fprintf(stderr, "errore nell'apertura del file\n");
+}
+while(fgets(q1,254,Fileq))
+printf("%s\n", strstr(q1, q));
+///a.class:::0 r
+FILE* Filer;
+if((Filer = fopen("Search results.txt", "r"))==NULL)
+{	fprintf(stderr, "errore nell'apertura del file\n");
+}
+while(fgets(r1,254,Filer))
+printf("%s\n", strstr(r1, r));
+//hi.a2 s
+FILE* Files;
+if((Files = fopen("Search results.txt", "r"))==NULL)
+{	fprintf(stderr, "errore nell'apertura del file\n");
+}
+while(fgets(s1,254,Files))
+printf("%s\n", strstr(s1, s));
+//0SO1Lk2KASxzsd t
+FILE* Filet;
+if((Filet = fopen("Search results.txt", "r"))==NULL)
+{	fprintf(stderr, "errore nell'apertura del file\n");
+}
+while(fgets(t1,254,Filet))
+printf("%s\n", strstr(t1, t));
+//yCcADi u
+FILE* Fileu;
+if((Fileu = fopen("Search results.txt", "r"))==NULL)
+{	fprintf(stderr, "errore nell'apertura del file\n");
+}
+while(fgets(u1,254,Fileu))
+printf("%s\n", strstr(u1, u));
+//74.91.125.194 v 
+FILE* Filev;
+if((Filev = fopen("Search results.txt", "r"))==NULL)
+{	fprintf(stderr, "errore nell'apertura del file\n");
+}
+while(fgets(v1,254,Filev))
+printf("%s\n", strstr(v1, v));
+//kc(g9 w
+FILE* Filew;
+if((Filew = fopen("Search results.txt", "r"))==NULL)
+{	fprintf(stderr, "errore nell'apertura del file\n");
+}
+while(fgets(w1,254,Filew))
+printf("%s\n", strstr(w1, w));
+//kc((k x
+FILE* Filex;
+if((Filex = fopen("Search results.txt", "r"))==NULL)
+{	fprintf(stderr, "errore nell'apertura del file\n");
+}
+while(fgets(x1,254,Filex))
+printf("%s\n", strstr(x1, x));
+//JNativeHook y
+FILE* Filey;
+if((Filey = fopen("Search results.txt", "r"))==NULL)
+{	fprintf(stderr, "errore nell'apertura del file\n");
+}
+while(fgets(y1,254,Filey))
+printf("%s\n", strstr(y1, y));
+//144.217.241.181 k
+FILE* Filek;
+if((Filek = fopen("Search results.txt", "r"))==NULL)
+{	fprintf(stderr, "errore nell'apertura del file\n");
+}
+while(fgets(k1,254,Filek)) 
+printf("%s\n", strstr(k1, k)); 
+//tcpnodelaymod/COM1 j
+FILE* Filej;
+if((Filej = fopen("Search results.txt", "r"))==NULL)
+{	fprintf(stderr, "errore nell'apertura del file\n");
+}
+while(fgets(j1,254,Filej)) 
+printf("%s\n", strstr(j1, j)); 
+system("PAUSE");
+}
+
+
